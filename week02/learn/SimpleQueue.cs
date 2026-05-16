e4757606bc3b2da56f784bde76b83904823b7eb9
evenage <charue.vyago2019@gmail.com>
2026-05-16 21:07:02 +0200

    done learning activity

--- week02/learn/SimpleQueue.cs
+++ week02/learn/SimpleQueue.cs
@@ -1,5 +1,27 @@
-﻿public class SimpleQueue {
-    public static void Run() {
+﻿using System;
+using System.Collections.Generic;
+
+public class SimpleQueue
+{
+    private readonly List<int> _queue = new();
+
+    public void Enqueue(int value)
+    {
+        _queue.Add(value);
+    }
+
+    public int Dequeue()
+    {
+        if (_queue.Count <= 0)
+            throw new InvalidOperationException("Queue is empty");
+
+        var value = _queue[0];
+        _queue.RemoveAt(0);
+        return value;
+    }
+    public static void Run()
+    {
+       // int value;
         // Test Cases
 
         // Test 1
@@ -10,7 +32,7 @@
         queue.Enqueue(100);
         var value = queue.Dequeue();
         Console.WriteLine(value);
-        // Defect(s) Found:
+        // Defect(s) Found: None
 
         Console.WriteLine("------------");
 
@@ -37,37 +59,27 @@
         // Expected Result: An exception should be raised
         Console.WriteLine("Test 3");
         queue = new SimpleQueue();
-        try {
+        try
+        {
             queue.Dequeue();
             Console.WriteLine("Oops ... This shouldn't have worked.");
         }
-        catch (IndexOutOfRangeException) {
+        catch (InvalidOperationException)
+        {
             Console.WriteLine("I got the exception as expected.");
         }
-        // Defect(s) Found: 
     }
 
-    private readonly List<int> _queue = new();
+    public List<int> Dequeue(int count)
+    {
+        if (count <= 0)
+            throw new ArgumentOutOfRangeException(nameof(count), "Count must be positive.");
 
-    /// <summary>
-    /// Enqueue the value provided into the queue
-    /// </summary>
-    /// <param name="value">Integer value to add to the queue</param>
-    private void Enqueue(int value) {
-        _queue.Insert(0, value);
-    }
+        if (_queue.Count < count)
+            throw new InvalidOperationException("Not enough items in the queue.");
 
-    /// <summary>
-    /// Dequeue the next value and return it
-    /// </summary>
-    /// <exception cref="IndexOutOfRangeException">If queue is empty</exception>
-    /// <returns>First integer in the queue</returns>
-    private int Dequeue() {
-        if (_queue.Count <= 0)
-            throw new IndexOutOfRangeException();
-
-        var value = _queue[1];
-        _queue.RemoveAt(1);
-        return value;
+        var values = _queue.GetRange(0, count);
+        _queue.RemoveRange(0, count);
+        return values;
     }
 }
